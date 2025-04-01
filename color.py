c967ffb1d5226a19a8f0cdcee68a345eacab1244
hwelch-fle <91618355+hwelch-fle@users.noreply.github.com>
2025-04-01 14:17:37 -0400

    missed some optimizations if you were golfing

--- color.py
+++ color.py
@@ -1,10 +1,17 @@
-def is_primary(c): return c[:3].count(256) == 1 and c[:3].count(0) == 2
-def is_secondary(c): return c[:3].count(256) == 2 and c[:3].count(0) == 1
-def main():
+def is_primary(c): 
+    return c[:3].count(256) == 1 and c[:3].count(0) == 2
+def is_secondary(c): 
+    return c[:3].count(256) == 2 and c[:3].count(0) == 1
+
+def check_value(r: str):
+    try:
+        c = [int(i) for i in (r.split() + ['256'])[:4]]
+        print(f"\rRGB{c} is {'Primary.'*is_primary(c) + 'Secondary.'*is_secondary(c) or 'Neither.'}")
+    except ValueError:
+        print("\rInvalid input.")
+    
+    
+if __name__ == "__main__":
     while (r := input("Enter RGB: ")) != 'q':
-        try:
-            c = tuple(map(int, (r.split() + ['256'])[:4]))
-            print(f"\rRGB{c} is " + ("Primary." if is_primary(c) else "Secondary." if is_secondary(c) else "Neither."))
-        except ValueError: print("\rInvalid input.")
+        check_value(r)
     print("\rAborted.")
-if __name__ == "__main__": main()
